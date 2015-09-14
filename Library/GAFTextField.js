131a1e8f179f3eaa1a3cf2b630c1807a01235033
ivanavdieienko <ivanavdieienko@gmail.com>
2015-09-14 18:20:22 +0300

    input text field support.

--- Library/GAFTextField.js
+++ Library/GAFTextField.js
@@ -4,6 +4,7 @@ gaf.TextField = gaf.Object.extend
     // Private variables
 
     _className: "GAFTextField",
+    _textField: null,
     _gafproto: null,
     _config: null,
 
@@ -50,9 +51,69 @@ gaf.TextField = gaf.Object.extend
             x: 0 + (this._config.pivot.x / size.width),
             y: 1 - (this._config.pivot.y / size.height)
         };
-        this._textField = new cc.TextFieldTTF(this._config.text, size, alignment, fontStyle, this._config.size);
-        this._textField.setColorSpaceHolder(this._config.color);
+
+        if (this._config.editable)
+        {
+            this._textField = new ccui.TextField(this._config.text, fontStyle, this._config.size);
+            this._textField.setTouchEnabled(true);
+            this._textField.setTextColor(this._config.color);
+            this._textField.setPlaceHolderColor(this._config.color);
+            this._textField.setTextHorizontalAlignment(alignment);
+            this._textField.passwordEnabled = this._config.displayAsPassword;
+            this._textField._textFieldRenderer._dimensions = cc.size(size.width, size.height);
+
+            if (this._config.maxChars > 0)
+            {
+                this._textField.maxLengthEnabled = true;
+                this._textField.maxLength = this._config.maxChars; //TODO: it works for web only
+            }
+            if (this._config.bold)
+            {
+                this._textField._textFieldRenderer._setFontWeight("bold"); //TODO: it works for web only
+            }
+            if (this._config.italic)
+            {
+                this._textField._textFieldRenderer._setFontStyle("italic"); //TODO: it works for web only
+            }
+        }
+        else
+        {
+            this._textField = new cc.TextFieldTTF(this._config.text, size, 0, fontStyle, this._config.size);
+            this._textField.setHorizontalAlignment(alignment);
+            if (this._config.bold)
+            {
+                this._textField._setFontWeight("bold"); //TODO: it works for web only
+            }
+            if (this._config.italic)
+            {
+                this._textField._setFontStyle("italic"); //TODO: it works for web only
+            }
+            this._textField.setColorSpaceHolder(this._config.color);
+        }
+
         this._textField.setAnchorPoint(anchor);
         this.addChild(this._textField);
+    },
+
+    getText: function()
+    {
+        var text = this._textField.string;
+        if (text.length > 0)
+        {
+            return text;
+        }
+        return this._textField.getPlaceHolder();
+    },
+
+    setText: function(value)
+    {
+        if (this._config.editable)
+        {
+            this._textField.string = value;
+        }
+        this._textField.setPlaceHolder(value);
     }
 });
+
+var _p = gaf.TextField.prototype;
+cc.defineGetterSetter(_p, "text", _p.getText, _p.setText);
